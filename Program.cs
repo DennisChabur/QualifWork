97a2957639f55ac72e09e4e262e38db3e7f0038c
Den <workplacel@mail.ru>
2022-10-27 00:07:15 +0500

    created new repo and added two files - readme and program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] input = new string[10];
+for (int i = 0; i < 10; i++)
+{
+    input[i] = Console.ReadLine();
+}
+
+System.Console.Write("[");
+for (int i = 0; i < input.Length; i++)
+{
+    if(input[i].Length < 4) 
+        Console.Write($"{input[i]} ");
+}
+System.Console.Write("]");
